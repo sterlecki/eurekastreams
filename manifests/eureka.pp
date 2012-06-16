e6c7c1624bd765955c52cb13648bbda252d9c52e
Stephen Terlecki <sterlecki@gmail.com>
2012-06-16 14:47:12 -0400

    added in apache, mod_auth kerb, postgres and create database puppet resources

--- manifests/eureka.pp
+++ manifests/eureka.pp
@@ -1,13 +1,17 @@
 #start setting stuff up here
 #need to install apache, if it isn't already there
 
+#setting the global path for all exec's 
+
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
 exec{ "update apt packages":
 	command => "apt-get update",
 }
 
 package{ "tomcat6":
 	ensure => present,
-	require => "update apt packages"
+	require => Exec["update apt packages"],
 }
 
 service{ "tomcat6":
@@ -17,7 +21,27 @@ service{ "tomcat6":
 
 package{ "postgresql":
 	ensure => present,
-	require => "update apt packages"
+	require => Exec["update apt packages"]
+}
+
+package{ "memcached":
+	ensure => present,
+	require => Exec["update apt packages"],
+}
+
+package{ "apache2":
+	ensure => present,
+	require => Exec["update apt packages"],
+}
+
+package{ "libapache2-mod-auth-kerb":
+	ensure => present,
+	require => Package["apache2"],
+}
+
+exec{ "enable mod_authnz_ldap for apache2":
+	command => 'sudo ln -s /etc/apache2/mods-available/authnz_ldap.load /etc/apache2/mods-enabled/authnz_ldap.load',
+	require => Package["apache2"],
 }
 
 exec{ "create eurekstreams db":
@@ -26,7 +50,7 @@ exec{ "create eurekstreams db":
 }
 
 exec{ "create eurekastreams user":
-	command => 'sudo -u postgres psql -c "create user eurekastreams with password 'eurekastreams';" postgres'
+	command => 'sudo -u postgres psql -c "create user eurekastreams with password \'eurekastreams\';" postgres',
 	logoutput => true,
 	require => Package["postgresql"],
 }
